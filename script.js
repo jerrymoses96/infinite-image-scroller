5f4b6b05f9492dfeec31097ef7d6512543c601af
jerin <jerinkunnamkada@gmail.com>
2024-01-02 17:16:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+// Unsplash API
+const count = 30;
+const apiKey = "DNI-7hABwh5VhKbjrDNYiH9XfnVk5qyMbNi0WKhCjZo";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// check if all images were loaded
+
+function imageLoaded() {
+  console.log("image loaded");
+  imageLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log("ready=", ready);
+  }
+}
+
+// helper function to set attributes on DOM elements
+
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// create element for links & photos,Add to Dom
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("total images", totalImages);
+  photosArray.forEach((photo) => {
+    // create a <a> to link to unsplash
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // create <img> for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // check when each is finished loading
+
+    img.addEventListener("load", imageLoaded);
+
+    // add the image and link to the container
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Get photos from Unsplash Api
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    // catch error here
+  }
+}
+
+// check to see if scrolling reached the bottom
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+    console.log("load more");
+  }
+});
+
+// onload
+getPhotos();
